01bfc7d0f7abe618a3a5903c46bea64bac48c415
Daniel Hong <dhong9@users.noreply.github.com>
2022-04-16 22:28:27 -0700

    Multiple Tables (#4)
    Adds data generation of multiple tables

--- README.md
+++ README.md
@@ -11,6 +11,19 @@ The bulk fo the input data is defined in `config.json`. The fields are described
 
 Which folder contains students' first names and last names?
 
+### `outputFolder`
+
+Which folder should generated data files be outputted in?
+
+### `tables`
+
+Names of output files:
+* `students`: Student names and year they are from
+* `assignments`: Assignment scores for each student
+* `quizzes`: Quiz scores for each student
+* `exams`: Exam scores for each student
+* `weights`: Weight of each category
+
 ### `firstNames`
 
 Which file contains students' first names? Assumption is that each name is on its own line.
@@ -23,6 +36,14 @@ Which file contains students' last names? Assumption is that each name is on its
 
 How many homework assignments does each student get?
 
+### `quizzes`
+
+How many quizzes does each student get?
+
+### `exams`
+
+How many exams does each student get?
+
 ### `corruptRate`
 
 What is the probability that an assignment will _not_ get turned in?
@@ -34,6 +55,10 @@ These are the range of scores each grade of student gets. The grade orders are: 
 ### `scoreRules`
 
 These are the grade distributions defined as a list of probabilities. The list order starts from the lowest grade (F) to the highest grade (A). The total of the percentages _must_ sum to `100`.
+
+### `weights`
+
+These are the weights of each category (assignments, quizzes, exams) in precent. The sum fo the weights _must_ equal `100`.
 
 ### `yearRange`
 
--- dataGenerator.py
+++ dataGenerator.py
@@ -1,13 +1,58 @@
 import random, csv, json
 
 # Utility functions:
+
+# Reads JSON file into an object
 readConfig = lambda src: json.load(open(src))
+
+# Reads text file line by line and store them in a list
 readNames = lambda src: open(src, "r").read().split("\n")
+
+# Generates random data
+def genData(n, corruptRate):
+    scores = []
+    for name in names:
+        randRule = random.uniform(0, 100)
+        x = 0
+        randIndex = 0
+        for rule in scoreRules:
+            if x <= randRule <= x + rule:
+                scores.append([-1 if random.uniform(0, 100) < corruptRate else round(randScoreFuncs[randIndex]()) for i in range(n)])
+                break
+            x += rule
+            randIndex += 1
+    return scores
+
+# Outputs data to a CSV file
+def outputData(fileName, header, rows):
+    with open(fileName, "w", encoding="UTF8", newline="") as f:
+        writer = csv.writer(f)
+        
+        # Write header
+        writer.writerow(header)
+
+        # Write rows
+        writer.writerows(rows)
 
 # Read config file
 print("Loading configuration...")
 configData = readConfig("config.json")
 inputFolder = configData["inputFolder"]
+outputFolder = configData["outputFolder"]
+tables = configData["tables"]
+outputStudents = outputFolder + "/" + tables["students"]
+outputAssignments = outputFolder + "/" + tables["assignments"]
+outputQuizzes = outputFolder + "/" + tables["quizzes"]
+outputExams = outputFolder + "/" + tables["exams"]
+outputWeights = outputFolder + "/" + tables["weights"]
+numberOfHomeworks = configData["homeworks"]
+numberOfQuizzes = configData["quizzes"]
+numberOfExams = configData["exams"]
+corruptRate = configData["corruptRate"]
+weights = configData["weights"]
+assignmentWeight = weights["assignments"]
+quizWeight = weights["quizzes"]
+examWeight = weights["exams"]
 
 print("Generating data...")
 
@@ -37,28 +82,26 @@ scoreRules = configData["scoreRules"]
 randScoreFuncs = [lambda p=p: random.uniform(p[0], p[1]) for p in configData["percentageRanges"]]
 
 # Generate random scores
-numberOfEntries = configData["homeworks"] # Number of entries per student
-corruptRate = configData["corruptRate"] # Data corruption probability
-scores = []
-for name in names:
-    randRule = random.uniform(0, 100)
-    x = 0
-    randIndex = 0
-    for rule in scoreRules:
-        if x <= randRule <= x + rule:
-            scores.append([-1 if random.uniform(0, 100) < corruptRate else round(randScoreFuncs[randIndex]()) for i in range(numberOfEntries)])
-            break
-        x += rule
-        randIndex += 1
+homeworkScores = genData(numberOfHomeworks, corruptRate)
+quizScores = genData(numberOfQuizzes, corruptRate)
+examScores = genData(numberOfExams, corruptRate)
 
-header = ["Student ID", "Name", "Year"] + ["Assignment #" + str(i + 1) for i in range(numberOfEntries)]
+# Write tables to CSV files
+print("Writing generated data to files...")
 
-# Output data to a CSV file
-with open('sampleData.csv', 'w', encoding='UTF8', newline='') as f:
-    writer = csv.writer(f)
-    
-    # Write header
-    writer.writerow(header)
+# Student names with years
+outputData(outputStudents, ["Student ID", "Name", "Year"], [[ids[i], names[i], random.randint(yearMin, yearMax)] for i in range(numberOfStudents)])
 
-    # Write rows
-    writer.writerows([[ids[i], names[i], random.randint(yearMin, yearMax)] + scores[i] for i in range(numberOfStudents)])
+# Assignments
+outputData(outputAssignments, ["Student ID"] + ["Assignment " + str(i + 1) for i in range(numberOfHomeworks)], [[ids[i]] + homeworkScores[i] for i in range(numberOfStudents)])
+
+# Quizzes
+outputData(outputQuizzes, ["Student ID"] + ["Quiz " + str(i + 1) for i in range(numberOfQuizzes)], [[ids[i]] + quizScores[i] for i in range(numberOfStudents)])
+
+# Exams
+outputData(outputExams, ["Student ID"] + ["Exam " + str(i + 1) for i in range(numberOfExams)], [[ids[i]] + examScores[i] for i in range(numberOfExams)])
+
+# Weights
+outputData(outputWeights, ["Category", "Weight"], [["Assignments", assignmentWeight], ["Quizzes", quizWeight], ["Exams", examWeight]])
+
+print("Data has been writen to folder: " + outputFolder)
